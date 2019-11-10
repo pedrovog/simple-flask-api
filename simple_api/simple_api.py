f74c95d56f0a9b1a309658cd69753f0172bba97d
Pedro Gonçalves <pedro.vog@gmail.com>
2019-11-10 18:16:48 -0300

    Configuring readiness and liveness probes

--- simple_api/simple_api.py
+++ simple_api/simple_api.py
@@ -20,5 +20,21 @@ def home():
     finally:
         return Response(json.dumps(response), mimetype='application/json', status=200)
 
+@app.route('/ping')
+def ping():
+    try:
+        response = {
+            'message': 'pong!',
+            'status': 'OK'
+        }
+    except Exception as err:
+        response = {
+            'message': 'Oh no!. ERROR: {}'.format(err),
+            'status': 'ERROR'
+        }
+        return Response(json.dumps(response), mimetype='application/json', status=400)
+    else:
+        return Response(json.dumps(response), mimetype='application/json', status=200)
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8081)
